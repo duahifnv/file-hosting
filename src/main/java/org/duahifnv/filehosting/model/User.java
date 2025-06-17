3515895a7207b91a7815e6a3b17c0fd703add462
Максим Фоминцев <max135603@gmail.com>
2025-06-17 16:18:46 +0300

    Spring Data JPA configuration & user entity

---
+++ src/main/java/org/duahifnv/filehosting/model/User.java
@@ -0,0 +1,21 @@
+package org.duahifnv.filehosting.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
---
+++ src/main/java/org/duahifnv/filehosting/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package org.duahifnv.filehosting.repository;
+
+import org.duahifnv.filehosting.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
+}
