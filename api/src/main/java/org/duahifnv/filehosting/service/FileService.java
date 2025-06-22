a4e013a991c6635fe1cfa5054f7a918d3db0e5c2
Максим Фоминцев <max135603@gmail.com>
2025-06-22 19:26:50 +0300

    File service remove method

---
+++ api/src/main/java/org/duahifnv/filehosting/service/FileService.java
@@ -0,0 +1,53 @@
+package org.duahifnv.filehosting.service;
+
+import lombok.RequiredArgsConstructor;
+import org.duahifnv.filehosting.model.CryptoData;
+import org.duahifnv.filehosting.model.FileMeta;
+import org.duahifnv.filehosting.model.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class FileService {
+    private static final String UPLOADS_BUCKET_NAME = "user-uploads";
+
+    private final FileMetaService metaService;
+    private final FileCryptoService cryptoService;
+    private final MinioService minioService;
+
+    public Optional<byte[]> downloadFile(UUID id, User user) throws Exception {
+        var metaOptional = metaService.findById(id, user);
+        if (metaOptional.isEmpty()) {
+            return Optional.empty();
+        }
+        var meta = metaOptional.get();
+
+        byte[] fileBytes = minioService.downloadObject(meta);
+        var cryptoData = new CryptoData(fileBytes, meta.getEncryptionKey(), meta.getIv());
+        return Optional.of(cryptoService.decryptData(cryptoData));
+    }
+
+    @Transactional
+    public void uploadFile(MultipartFile file, User user) throws Exception {
+        CryptoData cryptoData = cryptoService.encryptStream(file.getInputStream());
+        FileMeta fileMeta = FileMeta.of(file, UPLOADS_BUCKET_NAME, cryptoData, user);
+
+        metaService.save(fileMeta);
+        minioService.uploadObject(fileMeta, cryptoData.bytes());
+    }
+
+    @Transactional
+    public void removeFile(UUID id, User user) throws Exception {
+        Optional<FileMeta> metaOptional = metaService.findById(id, user);
+
+        if (metaOptional.isPresent()) {
+            minioService.removeObject(metaOptional.get());
+            metaService.remove(metaOptional.get());
+        }
+    }
+}
---
+++ api/src/test/java/org/duahifnv/filehosting/service/FileServiceTest.java
@@ -0,0 +1,124 @@
+package org.duahifnv.filehosting.service;
+
+import org.duahifnv.filehosting.model.CryptoData;
+import org.duahifnv.filehosting.model.FileMeta;
+import org.duahifnv.filehosting.model.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class FileServiceTest {
+    private static final byte[] testBytes = new byte[] {10, -23, 12};
+    private static final UUID fileId = UUID.fromString("79f3e08b-6d7c-44d4-bb7f-d502eab194de");
+
+    @InjectMocks
+    private FileService fileService;
+
+    @Mock
+    private FileMetaService metaService;
+    @Mock
+    private FileCryptoService cryptoService;
+    @Mock
+    private MinioService minioService;
+
+    @Test
+    void downloadFile_shouldReturnFileBytes_withExistingFile() throws Exception {
+        // given
+        var user = mock(User.class);
+
+        var fileMeta = mock(FileMeta.class);
+
+        when(metaService.findById(fileId, user)).thenReturn(Optional.of(fileMeta));
+        when(minioService.downloadObject(fileMeta)).thenReturn(testBytes);
+
+        var decryptedBytes = testBytes;
+        when(cryptoService.decryptData(any(CryptoData.class))).thenReturn(decryptedBytes);
+
+        // when
+        var result = fileService.downloadFile(fileId, user);
+
+        // then
+        verify(metaService).findById(fileId, user);
+        verify(minioService).downloadObject(fileMeta);
+        verify(cryptoService).decryptData(any(CryptoData.class));
+        assertThat(result).isPresent().hasValue(decryptedBytes);
+    }
+
+    @Test
+    void downloadFile_shouldThrowException_withNonExistingFile() throws Exception {
+        // given
+        var user = mock(User.class);
+
+        when(metaService.findById(fileId, user)).thenReturn(Optional.empty());
+
+        // when
+        var result = fileService.downloadFile(fileId, user);
+
+        // then
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void uploadFile_shouldUploadToMinioAndSaveMeta() throws Exception {
+        // given
+        var file = mock(MultipartFile.class);
+        var fileStream = mock(InputStream.class);
+
+        when(file.getInputStream()).thenReturn(fileStream);
+
+        var cryptoData = mock(CryptoData.class);
+        when(cryptoData.bytes()).thenReturn(testBytes);
+
+        when(cryptoService.encryptStream(fileStream)).thenReturn(cryptoData);
+
+        var user = mock(User.class);
+
+        // when
+        fileService.uploadFile(file, user);
+
+        // then
+        verify(metaService, times(1)).save(any(FileMeta.class));
+        verify(minioService, times(1)).uploadObject(any(FileMeta.class), eq(testBytes));
+    }
+
+    @Test
+    void removeFile_shouldRemoveMetaAndRemoveMinioObject_withExistingFile() throws Exception {
+        // given
+        var user = mock(User.class);
+        var fileMeta = mock(FileMeta.class);
+        when(metaService.findById(fileId, user)).thenReturn(Optional.of(fileMeta));
+
+        // when
+        fileService.removeFile(fileId, user);
+
+        // then
+        verify(minioService, times(1)).removeObject(fileMeta);
+        verify(metaService, times(1)).remove(fileMeta);
+    }
+
+    @Test
+    void removeFile_shouldDoNothing_withNonExistingFile() throws Exception {
+        // given
+        var user = mock(User.class);
+        when(metaService.findById(fileId, user)).thenReturn(Optional.empty());
+
+        // when
+        fileService.removeFile(fileId, user);
+
+        // then
+        verify(minioService, never()).removeObject(any(FileMeta.class));
+        verify(metaService, never()).remove(any(FileMeta.class));
+    }
+}
