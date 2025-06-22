fd8eac87b75f7f472a0ec87bf8a3decfb1a14b81
Максим Фоминцев <max135603@gmail.com>
2025-06-22 18:04:07 +0300

    MinioService methods signature refactor

--- api/src/main/java/org/duahifnv/filehosting/service/MinioService.java
+++ api/src/main/java/org/duahifnv/filehosting/service/MinioService.java
@@ -1,12 +1,13 @@
 package org.duahifnv.filehosting.service;
 
+import io.minio.GetObjectArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
-import io.minio.GetObjectArgs;
+import org.duahifnv.filehosting.model.FileMeta;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.ByteArrayInputStream;
 
 @Service
 public class MinioService {
@@ -16,23 +17,27 @@ public class MinioService {
         this.minioClient = minioClient;
     }
 
-    public void uploadObject(String bucketName, String objectName, InputStream inputStream, long size, String contentType) throws Exception {
-        minioClient.putObject(
-            PutObjectArgs.builder()
-                .bucket(bucketName)
-                .object(objectName)
-                .stream(inputStream, size, -1)
-                .contentType(contentType)
-                .build()
-        );
+    @Transactional
+    public void uploadObject(FileMeta metaData, byte[] bytes) throws Exception {
+        try (var byteInput = new ByteArrayInputStream(bytes)) {
+            minioClient.putObject(
+                PutObjectArgs.builder()
+                    .bucket(metaData.getBucket())
+                    .object(metaData.getObjectPath())
+                    .stream(byteInput, metaData.getSize(), -1)
+                    .contentType(metaData.getContentType())
+                    .build()
+            );
+        }
     }
 
-    public InputStream downloadObject(String bucketName, String objectName) throws Exception {
+    @Transactional
+    public byte[] downloadObject(FileMeta metaData) throws Exception {
         return minioClient.getObject(
             GetObjectArgs.builder()
-                .bucket(bucketName)
-                .object(objectName)
+                .bucket(metaData.getBucket())
+                .object(metaData.getObjectPath())
                 .build()
-        );
+        ).readAllBytes();
     }
 }
--- api/src/test/java/org/duahifnv/filehosting/service/MinioServiceTest.java
+++ api/src/test/java/org/duahifnv/filehosting/service/MinioServiceTest.java
@@ -1,16 +1,17 @@
 package org.duahifnv.filehosting.service;
 
 import io.minio.GetObjectArgs;
+import io.minio.GetObjectResponse;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
+import org.duahifnv.filehosting.model.FileMeta;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.io.ByteArrayInputStream;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 
@@ -25,16 +26,18 @@ public class MinioServiceTest {
     @Test
     public void uploadObject_shouldCallMinioClient() throws Exception {
         // given
-        var bucketName = "test-bucket";
-        var objectName = "test-object";
-        var inputStream = new ByteArrayInputStream("test-content".getBytes());
-        var size = 10;
-        var contentType = "text/plain";
+        var fileMeta = mock(FileMeta.class);
+        var fileBytes = new byte[] {-10, 42, 52};
+
+        when(fileMeta.getBucket()).thenReturn("test-bucket");
+        when(fileMeta.getObjectPath()).thenReturn("test-object");
+        when(fileMeta.getSize()).thenReturn(10L);
+        when(fileMeta.getContentType()).thenReturn("text/plain");
 
         when(minioClient.putObject(any(PutObjectArgs.class))).thenReturn(null);
 
         // when
-        minioService.uploadObject(bucketName, objectName, inputStream, size, contentType);
+        minioService.uploadObject(fileMeta, fileBytes);
 
         // then
         verify(minioClient, times(1)).putObject(any(PutObjectArgs.class));
@@ -43,14 +46,21 @@ public class MinioServiceTest {
     @Test
     public void downloadObject_shouldCallMinioClient() throws Exception {
         // given
-        var bucketName = "test-bucket";
-        var objectName = "test-object";
-        when(minioClient.getObject(any(GetObjectArgs.class))).thenReturn(null);
+        var fileMeta = mock(FileMeta.class);
+        var fileBytes = new byte[] {-10, 42, 52};
+
+        when(fileMeta.getBucket()).thenReturn("test-bucket");
+        when(fileMeta.getObjectPath()).thenReturn("test-object");
+
+        var objectResponse = mock(GetObjectResponse.class);
+        when(objectResponse.readAllBytes()).thenReturn(fileBytes);
+        when(minioClient.getObject(any(GetObjectArgs.class))).thenReturn(objectResponse);
 
         // when
-        var result = minioService.downloadObject(bucketName, objectName);
+        var result = minioService.downloadObject(fileMeta);
 
         // then
+        assertThat(result).isEqualTo(fileBytes);
         verify(minioClient, times(1)).getObject(any(GetObjectArgs.class));
     }
 }
